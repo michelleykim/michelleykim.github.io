698b44fa07a62099a2d537dba31f78a526d63847
michelleykim <33190160+michelleykim@users.noreply.github.com>
2020-07-09 04:50:34 -0600

    Projects toggle view implemented. General design cleaned.

---
+++ js/toggleWorks.js
@@ -0,0 +1,8 @@
+function toggleWorks() {
+    let x = document.getElementById("works");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
