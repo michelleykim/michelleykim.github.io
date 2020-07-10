0b313c8236f220813ebe34f50f4e7d8cd4421443
michelleykim <33190160+michelleykim@users.noreply.github.com>
2020-07-10 04:18:04 -0600

    contacts implemented

---
+++ js/copyEmail.js
@@ -0,0 +1,9 @@
+function copyEmail() {
+    const textarea = document.createElement('textarea');
+    document.body.appendChild(textarea);
+    textarea.value = "yeojin011016@gmail.com";
+    textarea.select();
+    textarea.setSelectionRange(0, 99999);
+    document.execCommand('copy');
+    document.body.removeChild(textarea);
+};
