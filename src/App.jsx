b09a00e768bd7625c928c954794e899d66d009c6
michelleykim <yeojin011016@gmail.com>
2023-05-23 03:38:53 -0700

    repo cleanup and website construction notice

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+	return (
+		<div className="App">
+			<h1>The website is under construction! Please return later!</h1>
+		</div>
+	);
+};
+
+export default App;
