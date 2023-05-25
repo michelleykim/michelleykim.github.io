216419ce05f416f49636ad9cd204c7f171b10327
michelleykim <yeojin011016@gmail.com>
2023-05-24 18:55:16 -0700

    add downwards grabbing animation

--- src/components/ClawGame.jsx
+++ src/components/ClawGame.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import "./ClawGame.css";
 import ClawMachineImg from "../assets/claw machine.svg";
-import ClawBodyImg from "../assets/claw.svg";
+import BodyImg from "../assets/body.svg";
+import HingeImg from "../assets/hinge.svg";
+import RodImg from "../assets/rod.svg";
 import ClawLImg from "../assets/clawL.svg";
 import ClawRImg from "../assets/clawR.svg";
 import ButtonLImg from "../assets/buttonL.svg";
@@ -10,46 +12,132 @@ import ButtonPImg from "../assets/buttonP.svg";
 
 const ClawGame = () => {
 	const clawBody = React.createRef();
+	const clawHinge = React.createRef();
+	const clawRod = React.createRef();
 	const clawL = React.createRef();
 	const clawR = React.createRef();
 	const rate = 30;
-	const diff = 62;
-	let clawXPos = 0;
-	let clawBodyXPos = 0;
+	const rate0 = 85;
 
-	const moveClaw = (dir) => {
+	let clawLXPos = 0;
+	let clawRXPos = 0;
+	let clawBodyXPos = 0;
+	let clawHingeXPos = 0;
+	let clawRodXPos = 0;
+
+	let clawHingeYPos = 0;
+	let clawYPos = 0;
+	let clawRodYPos = 0;
+	let clawRodSize = 0;
+
+	let rotateL = 0;
+	let rotateR = 0;
+
+	const xClaw = (e, dir) => {
+		e.target.style.pointerEvents = "none";
+
 		if (dir === "L") {
 			clawBodyXPos -= rate;
-			clawXPos -= diff + rate;
+			clawLXPos -= rate0;
+			clawRXPos -= rate0;
+			clawHingeXPos -= rate0 - 1;
+			clawRodXPos -= 140;
 		}
 		if (dir === "R") {
 			clawBodyXPos += rate;
-			clawXPos += diff + rate;
+			clawLXPos += rate0;
+			clawRXPos += rate0;
+			clawHingeXPos += rate0;
+			clawRodXPos += 140;
 		}
 
-		if (clawBodyXPos < -6 * rate) {
-			clawBodyXPos = -6 * rate;
-			clawXPos = -6 * (diff + rate);
+		if (clawBodyXPos < -7 * rate) {
+			clawBodyXPos = -7 * rate;
+			clawLXPos = -7 * rate0;
+			clawRXPos = -7 * rate0;
+			clawHingeXPos = -7 * rate0;
+			clawRodXPos = -7 * 140;
 		}
 		if (clawBodyXPos > 6 * rate) {
 			clawBodyXPos = 7 * rate - 5;
-			clawXPos = 7 * (diff + rate);
+			clawLXPos = 7 * rate0;
+			clawRXPos = 7 * rate0;
+			clawHingeXPos = 7 * rate0 - 4;
+			clawRodXPos = 7 * 140;
 		}
 
+		clawBody.current.style.transition = `transform 0.5s`;
+		clawHinge.current.style.transition = `transform 0.5s`;
+		clawRod.current.style.transition = `transform 0.5s`;
+		clawL.current.style.transition = `transform 0.5s`;
+		clawR.current.style.transition = `transform 0.5s`;
+
+		setTimeout(function () {
+			moveClaw();
+			e.target.style.pointerEvents = "";
+		}, 500);
+	};
+
+	const yClaw = (e) => {
+		e.target.style.pointerEvents = "none";
+
+		// go down
+		clawHingeYPos += 700;
+		clawYPos += 350;
+		clawRodYPos += 330;
+		clawRodSize = 10;
+		rotateL = 0.2;
+		rotateR = -0.2;
+		clawYPos -= 45;
+		clawLXPos -= 70;
+		clawRXPos += 65;
+
+		clawHinge.current.style.transition = `transform 1s`;
+		clawL.current.style.transition = `transform 1s`;
+		clawR.current.style.transition = `transform 1s`;
+		clawRod.current.style.transition = `transform 1s`;
+
+		moveClaw();
+
+		setTimeout(function () {
+			rotateL -= 0.2;
+			rotateR += 0.2;
+			clawYPos += 45;
+			clawLXPos += 70;
+			clawRXPos -= 65;
+
+			moveClaw();
+		}, 1000);
+
+		// come up
+		setTimeout(function () {
+			clawHingeYPos -= 700;
+			clawYPos -= 350;
+			clawRodYPos -= 330;
+			clawRodSize = 1;
+
+			moveClaw();
+
+			setTimeout(function () {
+				e.target.style.pointerEvents = "";
+			}, 2000);
+		}, 2000);
+	};
+
+	const moveClaw = () => {
 		clawBody.current.style.transform = `translateX(${clawBodyXPos}%)`;
-		clawBody.current.style.transition = `transform 0.3s ease-out`;
-
-		clawL.current.style.transform = `translateX(${clawXPos}%)`;
-		clawL.current.style.transition = `transform 0.3s ease-out`;
-
-		clawR.current.style.transform = `translateX(${clawXPos}%)`;
-		clawR.current.style.transition = `transform 0.3s ease-out`;
+		clawRod.current.style.transform = `translate(${clawRodXPos}%, ${clawRodYPos}%) scaleY(${clawRodSize})`;
+		clawHinge.current.style.transform = `translate(${clawHingeXPos}%, ${clawHingeYPos}%)`;
+		clawL.current.style.transform = `translate(${clawLXPos}%, ${clawYPos}%) rotate(${rotateL}turn)`;
+		clawR.current.style.transform = `translate(${clawRXPos}%, ${clawYPos}%) rotate(${rotateR}turn)`;
 	};
 
 	return (
 		<div className="Game">
 			<img id="ClawMachine" src={ClawMachineImg} alt="claw machine" />
-			<img id="ClawBody" ref={clawBody} src={ClawBodyImg} alt="claw body" />
+			<img id="ClawBody" ref={clawBody} src={BodyImg} alt="claw body" />
+			<img id="ClawHinge" ref={clawHinge} src={HingeImg} alt="claw hinge" />
+			<img id="ClawRod" ref={clawRod} src={RodImg} alt="claw rod" />
 			<img id="ClawL" ref={clawL} src={ClawLImg} alt="left claw" />
 			<img id="ClawR" ref={clawR} src={ClawRImg} alt="right claw" />
 
@@ -57,15 +145,20 @@ const ClawGame = () => {
 				id="ButtonL"
 				src={ButtonLImg}
 				alt="left button"
-				onClick={() => moveClaw("L")}
+				onClick={(e) => xClaw(e, "L")}
 			/>
 			<img
 				id="ButtonR"
 				src={ButtonRImg}
 				alt="right button"
-				onClick={() => moveClaw("R")}
+				onClick={(e) => xClaw(e, "R")}
 			/>
-			<img id="ButtonP" src={ButtonPImg} alt="play button" />
+			<img
+				id="ButtonP"
+				src={ButtonPImg}
+				alt="play button"
+				onClick={(e) => yClaw(e)}
+			/>
 		</div>
 	);
 };
