f4924180c894757cc0086fde3ca26f48480f8d63
michelleykim <yeojin011016@gmail.com>
2023-07-18 16:52:58 -0700

    better responsiveness

--- src/components/Chip.jsx
+++ src/components/Chip.jsx
@@ -1,16 +1,8 @@
 import React from "react";
+import "./Chip.css";
 
 const Chip = ({ text }) => {
-	const chip = {
-		border: "2px solid",
-		borderRadius: "10px",
-		padding: "8px 12px",
-		marginRight: "12px",
-		marginBottom: "8px",
-		fontSize: "16px",
-	};
-
-	return <span style={chip}>{text}</span>;
+	return <span className="chip">{text}</span>;
 };
 
 export default Chip;
